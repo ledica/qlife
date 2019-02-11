9590c99d9762f4cd5e95fe907ffafa18d43880dd
Meirelles <borgesdemeirelles@gmail.com>
2019-02-11 16:34:16 -0200

    Fazendo o app aproveitar o usuario logado na tela inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -27,6 +27,18 @@ export class HomePage {
     this.menu.swipeEnable(false);
   }
 
+
+  ionViewDidEnter() {
+    this.auth.refreshToken()
+      .subscribe(response => {
+        this.auth.successfulLogin(response.headers.get('Authorization'));
+        this.navCtrl.setRoot('SelecaoDashBoardPage');
+      },
+      error => {});  
+  }
+
+
+
   ionViewDidLeave() {
     this.menu.swipeEnable(true);
   }
