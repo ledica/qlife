b1fff1539291607b130472c3abb920e3cd9e1e1c
Meirelles <borgesdemeirelles@gmail.com>
2019-02-11 16:23:21 -0200

    Tratamento padrao para outros erros com Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,38 +2,77 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
 import { StorageService } from '../services/storage.service';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService) {
+    constructor(public storage: StorageService, public alertCtrl: AlertController) {
+
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req)
-        .catch((error, caught) => {
-            let errorObj = error;
-            if (errorObj.error) {
-                errorObj = errorObj.error;
-            }
-            if (!errorObj.status) {
-                errorObj = JSON.parse(errorObj);
-            }
-            console.log("Erro detectado pelo interceptor:");
-            console.log(errorObj);
+            .catch((error, caught) => {
+                let errorObj = error;
+                if (errorObj.error) {
+                    errorObj = errorObj.error;
+                }
+                if (!errorObj.status) {
+                    errorObj = JSON.parse(errorObj);
+                }
+                console.log("Erro detectado pelo interceptor:");
+                console.log(errorObj);
 
-            switch(errorObj.status) {
-                case 403:
-                this.handle403();
-                break;
-            }
+                switch (errorObj.status) {
+                    case 401:
+                        this.handle401();
+                        break;
 
-            return Observable.throw(errorObj);
-        }) as any;
+
+                    case 403:
+                        this.handle403();
+                        break;
+
+
+                    default:
+                        this.handleDefaultEror(errorObj);
+                }
+
+                return Observable.throw(errorObj);
+            }) as any;
     }
 
     handle403() {
         this.storage.setLocalUser(null);
+    }
+
+    handle401() {
+        let alert = this.alertCtrl.create({
+            title: 'Erro 401: falha de autenticação',
+            message: 'Email ou senha incorretos',
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
+    }
+
+    handleDefaultEror(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
     }
 
 }
